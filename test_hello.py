337b859951330182d16720f06b02f0573fa833ae
Juan Botero <63695358+juanboterotech@users.noreply.github.com>
2023-11-16 04:16:47 +0000

    Typo in test_hello

--- test_hello.py
+++ test_hello.py
@@ -3,7 +3,7 @@ from hello import search
 
 
 def test_search():
-    runner = CliRunner
+    runner = CliRunner()
     result = runner.invoke(search, ["--path", ".", "--ftype", "py"])
     assert result.exit_code == 0
     assert ".py" in result.output
