9374b89616ef3996c36499cdccdbb5ae1a075730
Juan Botero <63695358+juanboterotech@users.noreply.github.com>
2023-11-16 04:03:45 +0000

    Adding the tests

--- hello.py
+++ hello.py
@@ -1,2 +1,24 @@
-def hi():
-    return "Hi"
+#!/usr/bin/env python
+import click
+import glob
+
+
+@click.command()
+@click.option(
+    "--path",
+    prompt="Path to search CVS files",
+    help="This is the path to search for files: /tmp",
+)
+@click.option(
+    "--ftype", prompt="Pass in the type of file", help="Pass in the type file: i.e csv"
+)
+def search(path, ftype):
+    """This is a tool that searches for patterns like *.csv"""
+    results = glob.glob(f"{path}/*.{ftype}")
+    click.echo(click.style("Found matches:", fg="red"))
+    for result in results:
+        click.echo(click.style(f"{result}", bg="blue", fg="white"))
+
+
+if __name__ == "__main__":
+    search()
